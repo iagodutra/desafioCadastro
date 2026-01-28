0c66f8ae1d8e753c0e16851b0beceda4be617e2b
Iago Dutra Garcia <iagodutra270@gmail.com>
2026-01-27 21:17:11 -0300

    feat: Inserção de Menu e tratamente de entrada não permitida

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,42 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Formulario form = new Formulario();
         form.IniciarFormulario();
         form.LerFormulario();
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+
+        while (true){
+
+
+            System.out.println("1. Cadastrar um novo pet");
+            System.out.println("2. Alterar os dados do pet cadastrado");
+            System.out.println("3. Deletar um pet cadastrado");
+            System.out.println("4. Listar todos os pets cadastrados");
+            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6. Sair");
+
+            System.out.print("Digite qual ação deseja fazer: ");
+            try {
+                input = scanner.nextInt();
+            } catch (InputMismatchException exception){
+                System.out.println("É necessário que você digite um número! ");
+                System.out.println(" ");
+                scanner.nextLine();
+                continue;
+            }
+
+            if (input == 6){
+                break;
+            }
+
+        }
+
     }
 }
